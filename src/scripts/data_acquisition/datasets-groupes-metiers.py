846ddcc28bfd34b5b3eea9c610e61a64bece262a
Asma <rachidi.asm@gmail.com>
2023-11-08 16:47:10 +0100

    Ajout script de récupération des groupes métiers

--- src/scripts/data_acquisition/datasets-groupes-metiers.py
+++ src/scripts/data_acquisition/datasets-groupes-metiers.py
@@ -2,8 +2,15 @@ import requests
 import json
 import csv
 
-APIKEY=""
-headers = {"content-type": "application/json", "Authorization": f"Apikey {APIKEY}"}
+# Récupérer le host, le nom d'utilisateur et le mot de passe à partir des variables d'environnement situées dans le fichier de conf.
+def load_db_config():
+    with open('../../../config.json') as config_file:
+        return json.load(config_file)
+      
+config = load_db_config()
+api_key = config['API_KEY']
+    
+headers = {"content-type": "application/json", "Authorization": f"Apikey {api_key}"}
 
 output_rows = []
 offset = 0
@@ -37,7 +44,7 @@ while 1==1:
       dataset['metadata']['admin']['groupe-metier']['value'][0] if 'admin' in dataset['metadata'] and 'groupe-metier' in dataset['metadata']['admin'] else None,
       f"https://data.economie.gouv.fr/backoffice/catalog/datasets/{dataset['dataset_id']}/#information"])
 
-with open('datasets_groupes_metiers_20231030.csv', 'w') as csvfile:
+with open('../../../data/raw/data_acquisition/extraction_groupes_metiers/datasets_groupes_metiers_20231030.csv', 'w') as csvfile:
   writer = csv.writer(csvfile, delimiter=';')
   writer.writerow(['title', 'dataset_id', 'is_restricted', 'is_published', 'groupe-metier', 'url'])
   writer.writerows(output_rows)
