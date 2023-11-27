5a57795a295d15ebda86f4723b95aa15b49809ee
Paul MOYSAN <paul.moysan@finances.gouv.fr>
2023-11-27 14:29:54 +0100

    test(auth): add test cases

--- tests/auth/auth_unit_test.py
+++ tests/auth/auth_unit_test.py
@@ -9,3 +9,35 @@ def test_retrieve_user():
     result = retrieve_user(repository=repository, username="jdoe")
     # Assert
     assert result.username == "jdoe"
+
+
+def test_valid_credentials():
+    pass
+
+
+def test_invalid_username():
+    pass
+
+
+def test_invalid_password():
+    pass
+
+
+def test_empty_credentials():
+    pass
+
+
+def test_missing_username():
+    pass
+
+
+def test_missing_password():
+    pass
+
+
+def test_long_username():
+    pass
+
+
+def test_long_password():
+    pass
