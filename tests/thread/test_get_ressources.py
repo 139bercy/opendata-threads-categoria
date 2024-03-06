684f1d492a294693e189ab685f946da6ecf1cab6
Asma RACHIDI <rachidi.asm@gmail.com>
2024-03-06 17:57:37 +0100

    feat : ajout de la fonction fetch_an_process qui permet d'automatiser le script

--- tests/thread/test_get_ressources.py
+++ tests/thread/test_get_ressources.py
@@ -11,18 +11,13 @@ from get_ressourcesVrai import APIDataFetcher
 @pytest.fixture
 def api_sources():
     return [
-        {"url": "https://www.data.gouv.fr/api/1/organizations/ministere-de-leconomie-des-finances-et-de-la-souverainete-industrielle-et-numerique"}
-        # Add more sources if needed
+        {"url": "https://www.data.gouv.fr/api/1/organizations/ministere-de-leconomie-des-finances-et-de-la-souverainete-industrielle-et-numerique"
+         }
     ]
 
 def test_create_instance(api_sources):
     #Arrange
-    """api_sources = [
-        {
-            "url": "https://www.data.gouv.fr/api/1/organizations/ministere-de-leconomie-des-finances-et-de-la-souverainete-industrielle-et-numerique"
-         }
-
-    ]"""
+    #api_sources
     #organization = "ministere-de-leconomie-des-finances-et-de-la-souverainete-industrielle-et-numerique"
     #Act
     api = APIDataFetcher(api_sources)
@@ -30,18 +25,17 @@ def test_create_instance(api_sources):
     assert isinstance(api, APIDataFetcher) 
 
 
-
 def test_save_data_to_json(api_sources, tmp_path):
     """tmp_path est un objet Path qui pointe vers un répertoire temporaire spécifique à chaque test. 
        On peut l'utiliser pour créer des fichiers temporaires, des répertoires temporaires, etc.
        Il sera automatiquement nettoyé à la fin de chaque test."""
+    # Arrange
     api= APIDataFetcher(api_sources, data_folder=tmp_path)
     resource = "datasetstest"
-
-    # Simulate fetching and processing data
     data = {"key": "value"}
+    # Act
     api.save_data_to_json(resource, data)
-
+    # Assert
     # Test if the file is created
     filename = f"{resource}.json"
     assert os.path.exists(tmp_path / filename)
@@ -56,3 +50,14 @@ def test_save_data_to_json(api_sources, tmp_path):
     os.remove(file_path)
 
 
+def test_fetch_and_process_data(api_sources, tmp_path):
+    # Arrange
+    api = APIDataFetcher(api_sources, data_folder=tmp_path)
+    # Act
+    with Mocker() as mocker:
+        url = api_sources[0]["url"]
+        mocker.get(url, json={"key": "value"})
+
+        api.fetch_and_process_data()
+    # Assert
+    filename = f"{api.resource}.json"
